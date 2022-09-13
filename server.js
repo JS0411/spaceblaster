b9c875d60a2265988eb0e30fa379a2fe663e1ccf
JS0411 <joseseijas0411@gmail.com>
2022-09-12 21:01:48 -0400

    test

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const port = process.env.PORT || 3001;
 const path = require("path");
 
 app.use(express.static('public'))
+var router = express.Router();
+
 app.get('/game', (req, res) => {
    
    res.sendFile(path.join(__dirname, '/public/game.html'))
 } )
+
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, '/public/index.html'))
+   res.render('index');
 } )
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
